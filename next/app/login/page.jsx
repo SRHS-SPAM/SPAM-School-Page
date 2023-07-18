2a4b4c56bb3522c1bc6db6bf2461e034129c039a
igiza1213 <igiza1213@gmail.com>
2023-07-18 22:09:25 +0900

    c

--- next/app/login/page.jsx
+++ next/app/login/page.jsx
@@ -4,14 +4,14 @@ import LoginPage from "./LoginPage";
 import styles from "./login.module.css";
 export default async function login() {
   return (
-    <>
+    <div>
       <Seo title="login" />
-      <header class={styles.login_header}>
-        <div class={styles.cafe_actions}>
+      <header className={styles.login_header}>
+        <div className={styles.cafe_actions}>
           <Arrow location="/" />
         </div>
       </header>
       <LoginPage></LoginPage>
-    </>
+    </div>
   );
 }
