6885695cea4cd854fd94787be171573573713692
igiza1213 <igiza1213@gmail.com>
2023-08-22 20:02:22 +0900

    Today's 30%completed calendar's
    +dnjeh && msg2324

---
+++ next/app/calendar/page.jsx
@@ -0,0 +1,12 @@
+import Arrow from "@/components/Arrow";
+import Seo from "@/components/Seo";
+import CalendarPage from "./CalendarPage";
+
+export default function Calender() {
+  return (
+    <>
+      <Seo title="Calendar" />
+      <CalendarPage></CalendarPage>
+    </>
+  );
+}
