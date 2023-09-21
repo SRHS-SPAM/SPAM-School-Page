3ce094ab799d7d68575c90f6353b05c41217ae23
igiza1213 <igiza1213@gmail.com>
2023-09-22 07:54:41 +0900

    Delete new.js

--- next/pages/api/community/new.js
+++
@@ -1,9 +0,0 @@
-import { connectDB } from "@/util/database";
-import { ObjectId } from "mongodb";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
-
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-  }
-}
