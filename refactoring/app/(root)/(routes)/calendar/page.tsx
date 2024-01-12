3c926dab9c2f297474aef4be688e7493d040f722
dnjeh <ehdnj.001@gmail.com>
2024-01-12 23:22:09 +0900

    ()++

--- refactoring/app/(root)/(routes)/calendar/page.tsx
+++ refactoring/app/(root)/(routes)/calendar/page.tsx
@@ -6,7 +6,14 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Plus } from "lucide-react";
 import EventItem from "../../_components/event-item";
 
-const Data = [{title: "입학식",
+interface EventItemProps {
+    title: string;
+    dateStart: string;
+    dateEnd: string;
+    content: any; //DOM 엘리먼트 뭐임??
+}
+
+const Data:EventItemProps[] = [{title: "입학식",
     dateStart: "3월 21일(수)",
     dateEnd: "3월 21일(수)",
     content: <>입학식<br/><br/>8시 20분까지 등교.<br/>최소 7시 기상<br/><br/>단축수업 30분 있음.</>},
@@ -29,7 +36,7 @@ export default function Home() {
             <div className="w-[43%] h-full drop-shadow-2xl bg-white rounded-2xl flex flex-col p-6 justify-stretch">
                 <p className="h-10">음력 2월 18일</p>
                 <ScrollArea className="h-full p-4">
-                    <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-8 mb-12">
                         {Data.map((ai, i)=>(
                             <EventItem key={i}
                             content={ai.content}
@@ -40,8 +47,8 @@ export default function Home() {
                     </div>
                 </ScrollArea>
                 <div className="h-[100px] flex justify-between items-end">
-                    <input className="w-9/12 h-3/5 rounded-full bg-slate-300 px-8 placeholder:text-white" placeholder="애 일정 추가"/>
-                    <Plus className="max-w-[100px] w-full h-[100px] text-white bg-yellow-300 rounded-full" />
+                    <input className="w-9/12 h-3/5 rounded-full bg-slate-300 px-8 placeholder:text-white text-white" placeholder="애 일정 추가"/>
+                    <Plus className="max-w-[90px] w-full h-[90px] text-white bg-yellow-300 rounded-full" />
                 </div>
             </div>
           </div>
--- refactoring/app/(root)/_components/event-item.tsx
+++ refactoring/app/(root)/_components/event-item.tsx
@@ -1,17 +1,25 @@
-import { ArrowRight } from "lucide-react";
+import { MoveRight } from "lucide-react";
 
-const EventItem = ({key, title, dateStart, dateEnd, content}) => {
+interface EventItemProps {
+    key?: number;
+    title: string;
+    dateStart: string;
+    dateEnd: string;
+    content: any; //DOM 엘리먼트 뭐임??
+}
+
+const EventItem = ({key, title, dateStart, dateEnd, content}:EventItemProps) => {
     return (
         <div className="flex flex-col gap-6">
             <p className="text-4xl font-black">{title}</p>
             <hr />
             <div className="flex items-center justify-center gap-12 text-2xl font-bold">
                 <p>{dateStart}</p>
-                <ArrowRight />
+                <MoveRight />
                 <p>{dateEnd}</p>
             </div>
             <hr />
-            <div className="p-4">
+            <div className="px-4">
                 {content}
             </div>
             <hr />
