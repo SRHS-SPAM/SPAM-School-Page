cbd2c83d75f1568165ca6626ac67bc44ccb061a3
dnjeh <ehdnj.001@gmail.com>
2024-01-19 22:04:49 +0900

    Today's

--- refactoring/app/(root)/_components/calendarYear.tsx
+++ refactoring/app/(root)/_components/calendarYear.tsx
@@ -7,9 +7,15 @@ interface CalendarYearProps {
   isMoving: boolean;
   sendYmd?: (tm: number, d: number) => void;
   setymd?: ([]) => void;
-  wrapRef: any //이건 또 뭘려나
+  wrapRef: any; //이건 또 뭘려나
 }
-const CalendarYear = ({ymd, isMoving, sendYmd, setymd, wrapRef}:CalendarYearProps) => {
+const CalendarYear = ({
+  ymd,
+  isMoving,
+  sendYmd,
+  setymd,
+  wrapRef,
+}: CalendarYearProps) => {
   const year = ymd[0];
   return (
     <div className="w-1/2 flex flex-col h-full">
@@ -17,12 +23,12 @@ const CalendarYear = ({ymd, isMoving, sendYmd, setymd, wrapRef}:CalendarYearProp
         <div
           ref={wrapRef}
           className={cn(
-            "h-[300%] w-full top-[-100%] absolute flex flex-col justify-stretch opacity-100",
+            "h-[233.33%] w-full top-[-66.66%] absolute flex flex-col justify-stretch opacity-100",
             isMoving && "transition-all ease-ease duration-300"
           )}
         >
           <CalendarYearAround
-            year={year-1}
+            year={year-10}
             isgray={true}
             rmline={2}
           />
@@ -31,10 +37,10 @@ const CalendarYear = ({ymd, isMoving, sendYmd, setymd, wrapRef}:CalendarYearProp
             sendYmd={isMoving || !setymd ? undefined : sendYmd}
           />
           <CalendarYearAround
-            year={year+1}
+            year={year+10}
             isgray={true}
             rmline={0}
-          />
+        />
         </div>
       </div>
     </div>
--- refactoring/app/(root)/_components/calendarYearAround.tsx
+++ refactoring/app/(root)/_components/calendarYearAround.tsx
@@ -1,70 +1,70 @@
 import { cn } from "@/lib/utils";
 import { useEffect } from "react";
 
-interface CalendarAroundProps {
+interface CalendarYearAroundProps {
   year: number;
-  month: number;
-  date: number;
   isgray?: boolean;
-  remove?: number[];
+  rmline?: number;
   sendYmd?: (tm: number, d: number) => void;
 }
-interface DatesFace {
-  date: number;
+interface YearsFace {
+  year: number;
   mon: "pre" | "cur" | "nxt";
 }
 const CalendarYearAround = ({
   year,
-  month,
-  date,
   isgray,
-  remove,
+  rmline,
   sendYmd,
-}: CalendarAroundProps) => {
-  const Dates: DatesFace[][] = [];
-  const currentDate = new Date();
-  const nowy = currentDate.getFullYear();
-  const nowm = currentDate.getMonth();
-  const nowd = currentDate.getDate();
-  const preMonthDate = new Date(year, month, 0);
-  const preMonthDay = preMonthDate.getDay();
-  const lastDay = new Date(year, month + 1, 0).getDate();
-  Dates.push([{ date: preMonthDate.getDate(), mon: "pre" }]);
+}: CalendarYearAroundProps) => {
+  const Years: YearsFace[][] = [];
+  const nowy = new Date().getFullYear();
 
-  for (let i = 0; i < preMonthDay; i++) {
-    Dates[0].unshift({ date: preMonthDate.getDate() - i - 1, mon: "pre" });
-  }
-  for (let i = 0; i < 6; i++) {
-    for (; Dates[i].length < 7; ) {
-      const len = Dates[i].length - 1;
-      if (Dates[i][len].mon == "pre") {
-        Dates[i].push({ date: 1, mon: "cur" });
-      } else if (Dates[i][len].date == lastDay) {
-        Dates[i].push({ date: 1, mon: "nxt" });
-      } else {
-        Dates[i].push({
-          date: Dates[i][len].date + 1,
-          mon: Dates[i][len].mon,
-        });
-      }
-    }
-    if (i < 5) {
-      if (Dates[i][6].mon == "pre") {
-        Dates.push([{ date: 1, mon: "cur" }]);
-      } else if (Dates[i][6].mon == "cur" && Dates[i][6].date == lastDay) {
-        Dates.push([{ date: 1, mon: "nxt" }]);
-      } else {
-        Dates.push([{ date: Dates[i][6].date + 1, mon: Dates[i][6].mon }]);
-      }
+  for (let i = 0; i < 3; i++) {
+    Years.push([]);
+    for (let j = 0; j < 4; j++) {
+      let t = i * 4 + j - 2;
+      Years[i].push({
+        year: t + year,
+        mon: t < 0 ? "pre" : t >= 10 ? "nxt" : "pre",
+      });
     }
   }
-  if (remove != null) {
-    Dates.splice(remove[0], remove[1]);
+
+  if (rmline != null) {
+    Years.splice(rmline, rmline);
   }
 
   return (
-    <>
-    </>
+    <div className="flex h-full w-full flex-col flex-1">
+      {Years.map((ai, i) => (
+        <div
+          key={i}
+          className="text-center text-black text-xl font-normal h-full items-center w-full flex justify-stretch"
+        >
+          {ai.map((aj, j) => (
+            <div
+              key={j}
+              className="flex items-center justify-center h-full w-full"
+            >
+              <div
+                className={cn(
+                  "cursor-pointer select-none hover:bg-yellow-200 w-[100px] h-[100px] flex items-center justify-center rounded-full ",
+                  (isgray || aj.mon != "cur") && "text-gray-300",
+                  aj.year == nowy && "bg-yellow-300",
+                  !isgray &&
+                    aj.mon == "cur" &&
+                    "bg-yellow-300 hover:bg-yellow-300"
+                )}
+                onClick={() => {}}
+              >
+                {aj.year}
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 };
 
