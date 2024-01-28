34c8d9f681bd50f7d0ac3915d759eafbffde5764
igiza1213 <igiza1213@gmail.com>
2024-01-28 15:23:48 +0900

    Update communityItem.tsx

--- refactoring/app/(root)/community/_components/communityItem.tsx
+++ refactoring/app/(root)/community/_components/communityItem.tsx
@@ -1,6 +1,7 @@
 import { Eye, MessageSquareIcon, ThumbsUp } from "lucide-react";
 import IconComp from "./IconComp";
 import React from "react";
+import Link from "next/link";
 
 interface IconCompProps {
   icon: React.JSX.Element;
@@ -24,13 +25,26 @@ const CommunityItem = ({
   views,
 }: DataFace) => {
   return (
-    <div className="flex gap-4">
-      {board && <div className="w-[95px] rounded-md border-[0.5px] border-gray-400 text-center">{board}</div>}
-      {title && <div className="w-full pl-2 pr-2">{title}</div>}
-      {good && <IconComp icon={<ThumbsUp className="h-4 w-4"/>} value={good} />}
-      {commentcnt && <IconComp icon={<MessageSquareIcon className="h-4 w-4"/>} value={commentcnt} />}
-      {views && <IconComp icon={<Eye className="h-4 w-4"/>} value={views} />}
-    </div>
+    <>
+      <Link href={link} className="flex gap-4">
+        {board && (
+          <div className="w-[95px] rounded-md border-[0.5px] border-gray-400 text-center">
+            {board}
+          </div>
+        )}
+        {title && <div className="w-full pl-2 pr-2">{title}</div>}
+        {good && (
+          <IconComp icon={<ThumbsUp className="h-4 w-4" />} value={good} />
+        )}
+        {commentcnt && (
+          <IconComp
+            icon={<MessageSquareIcon className="h-4 w-4" />}
+            value={commentcnt}
+          />
+        )}
+        {views && <IconComp icon={<Eye className="h-4 w-4" />} value={views} />}
+      </Link>
+    </>
   );
 };
 
